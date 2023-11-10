365f0ba28388ab37689863b368ede929a3ea0121
Heorhi Zaturkevich <konisgung@gmail.com>
2023-11-10 22:35:49 +0300

    Started writing sorting logic

---
+++ modules/favouritesSystem.js
@@ -0,0 +1,112 @@
+import {comments, saveComments} from './main.js';
+let toggleFavorites = false;
+
+export function favoritesRegistration(target) {
+    const favoritesComment = target.parentNode.parentNode.parentNode.parentNode;
+    const favoritesAnswer = target.parentNode.parentNode.parentNode;
+    const ParentAnswer = target.parentNode.parentNode.parentNode.parentNode.parentNode;
+    const indexAnswerParent = ParentAnswer.getAttribute('data-index');
+    const indexComment = favoritesComment.getAttribute('data-index');
+    const indexAnswer = favoritesAnswer.getAttribute('data-index'); 
+
+    const imageFavorites = target.parentNode.querySelector('.heart-svg');
+    const firstImage = '/images/Only-heart.svg'; 
+    const secondImage = '/images/Empty-heart.svg';
+    
+    if (favoritesComment.classList.value === 'static-comment') {
+        if (comments[indexComment].is_favorite === false) {
+            comments[indexComment].is_favorite = true;
+            imageFavorites.src = firstImage;
+        }
+        else if (comments[indexComment].is_favorite === true) {
+            comments[indexComment].is_favorite = false;
+            imageFavorites.src = secondImage;
+        }
+    }
+    else if (favoritesAnswer.classList.value === 'static-comment__reply') {
+        if (comments[indexAnswerParent].answers[indexAnswer].is_favorite === false) {
+            comments[indexAnswerParent].answers[indexAnswer].is_favorite = true;
+            imageFavorites.src = firstImage;
+        }
+        else if (comments[indexAnswerParent].answers[indexAnswer].is_favorite === true) {
+            comments[indexAnswerParent].answers[indexAnswer].is_favorite = false;
+            imageFavorites.src = secondImage;
+        }
+    }
+    saveComments();
+}
+
+export function pasteImageFavorites() {
+    const savedComments = JSON.parse(localStorage.getItem('comments'));
+    const firstImage = '/images/Only-heart.svg'; 
+    const secondImage = '/images/Empty-heart.svg';
+
+    if (savedComments) {
+        savedComments.forEach(function(comment, index) {
+            let commentObj = document.querySelector('[data-index="' + index + '"]');
+            const imageFavorites = commentObj.querySelector('.heart-svg');
+            
+            if (comment.is_favorite) {
+                imageFavorites.src = firstImage;
+            }
+            else if (!comment.is_favorite) {
+                imageFavorites.src = secondImage;
+            }
+            
+            comment.answers.forEach(function(answer, index) {
+                let answerObj = commentObj.querySelector('[data-index="' + index + '"]');
+                const answerImage = answerObj.querySelector('.heart-svg');
+    
+                if (answer.is_favorite) {
+                    answerImage.src = firstImage;
+                }
+                else if (!answer.is_favorite) {
+                    answerImage.src = secondImage;
+                }
+            }) 
+        })
+    }
+}
+
+export function drawFavorites() {
+    toggleFavorites = !toggleFavorites; // Переключение состояния
+
+    comments.forEach(function(comment, index) {
+        let commentObj = document.querySelector('[data-index="' + index + '"]');
+        const staticCommentFlex = commentObj.querySelector('.static-comment__flex');
+        const replyField = commentObj.querySelector('.reply-field')
+        const staticCommentReply = commentObj.querySelectorAll('.static-comment__reply');
+
+        if (toggleFavorites) {
+            if (comment.is_favorite !== true) {
+                staticCommentFlex.style.display = 'none';
+                replyField.style.marginTop = '0px';
+                    staticCommentReply.forEach((item) => {
+                        item.style.marginLeft = '0px';
+                    })
+            }
+            
+            comment.answers.forEach(function(answer, index) {
+                let answerObj = commentObj.querySelector('[data-index="' + index + '"]');
+                
+                if (answer.is_favorite !== true) {
+                    answerObj.style.display = 'none';
+                }
+            })
+        } 
+        else if (!toggleFavorites) {
+            
+            staticCommentFlex.style.display = 'flex';
+            replyField.style.marginTop = '30px';
+            staticCommentReply.forEach((item) => {
+                item.style.marginLeft = '88px';
+            })
+            
+            comment.answers.forEach(function(answer, index) {
+                let answerObj = commentObj.querySelector('[data-index="' + index + '"]');
+                answerObj.style.display = 'flex';
+            }) 
+            
+        }
+    })
+}
--- modules/main.js
+++ modules/main.js
@@ -2,9 +2,9 @@ import {commentName, commentBody, button, symbolsLimit, placeholder,} from './co
 import {handleInput, banImage, banHtmlTag} from './inputProcessing.js';
 import {timeConverter, numbOfComments} from './utils.js';
 import {ratingCount} from './rating.js';
+import {favoritesRegistration, pasteImageFavorites, drawFavorites} from './favouritesSystem.js';
 
 export let comments = [];
-let toggleFavorites = false;
 loadComments();
 
 document.querySelector('.send__btn').addEventListener('click',  function() {
@@ -21,7 +21,7 @@ document.querySelector('.send__btn').addEventListener('click',  function() {
     comments.push(comment);
 
     saveComments();
-    showComments();
+    showComments(comments);
     button.style.backgroundColor = '';
     symbolsLimit.innerHTML = `Макс. 1000 символов`;
     symbolsLimit.classList.remove('active');
@@ -37,14 +37,14 @@ export function saveComments() {
 
 function loadComments() {
     if (localStorage.getItem('comments')) comments = JSON.parse(localStorage.getItem('comments'));
-    showComments();
+    showComments(comments);
     pasteImageFavorites();
 }
 
-function showComments() {
+function showComments(array) {
     let commentField = document.getElementById('comment-field');
     let out = '';
-    comments.forEach(function (item, commentIndex ) {
+    array.forEach(function (item, commentIndex ) {
         out += `<div class="static-comment" data-index="${commentIndex}">
             <div class="static-comment__flex">
             <img src="./images/Max.png" alt="Max" class="avatar">
@@ -84,6 +84,7 @@ function showComments() {
     commentField.innerHTML = out;
     initAnswersEvent();
     numbOfComments();
+    pasteImageFavorites();
 }
 
 commentBody.addEventListener('input', (event) => handleInput(event));
@@ -196,105 +197,34 @@ document.addEventListener('click', function(event) {
     if (target.matches('.favourites')) {
         drawFavorites();
     }
+
+    if (target.matches('.filters')) {
+        const dropDown = document.querySelector('.drop-down');
+        const displayInfo = window.getComputedStyle(dropDown).display;
+
+        if (displayInfo === 'none') {
+            dropDown.style.display = 'block'
+            //target.style.transform = "rotate(180deg)";
+            sortingComments(dropDown);
+        }
+        else {
+            dropDown.style.display = 'none';
+            //target.style.transform = "rotate(3600deg)";
+        }
+    }
 });
 
-function favoritesRegistration(target) {
-    const favoritesComment = target.parentNode.parentNode.parentNode.parentNode;
-    const favoritesAnswer = target.parentNode.parentNode.parentNode;
-    const ParentAnswer = target.parentNode.parentNode.parentNode.parentNode.parentNode;
-    const indexAnswerParent = ParentAnswer.getAttribute('data-index');
-    const indexComment = favoritesComment.getAttribute('data-index');
-    const indexAnswer = favoritesAnswer.getAttribute('data-index'); 
+function sortingComments(dropDown) {
+    const replyField = document.querySelector('.comment-field')
 
-    const imageFavorites = target.parentNode.querySelector('.heart-svg');
-    const firstImage = '/images/Only-heart.svg'; 
-    const secondImage = '/images/Empty-heart.svg';
-    
-    if (favoritesComment.classList.value === 'static-comment') {
-        if (comments[indexComment].is_favorite === false) {
-            comments[indexComment].is_favorite = true;
-            imageFavorites.src = firstImage;
-        }
-        else if (comments[indexComment].is_favorite === true) {
-            comments[indexComment].is_favorite = false;
-            imageFavorites.src = secondImage;
-        }
-    }
-    else if (favoritesAnswer.classList.value === 'static-comment__reply') {
-        if (comments[indexAnswerParent].answers[indexAnswer].is_favorite === false) {
-            comments[indexAnswerParent].answers[indexAnswer].is_favorite = true;
-            imageFavorites.src = firstImage;
-        }
-        else if (comments[indexAnswerParent].answers[indexAnswer].is_favorite === true) {
-            comments[indexAnswerParent].answers[indexAnswer].is_favorite = false;
-            imageFavorites.src = secondImage;
-        }
-    }
-    saveComments();
-}
+    dropDown.addEventListener('click', function(event) {
+        const target = event.target;
+        const indexLi = target.getAttribute('data');
+        console.log(indexLi)
 
-function pasteImageFavorites() {
-    const savedComments = JSON.parse(localStorage.getItem('comments'));
-    const firstImage = '/images/Only-heart.svg'; 
-    const secondImage = '/images/Empty-heart.svg';
-
-    savedComments.forEach(function(comment, index) {
-        let commentObj = document.querySelector('[data-index="' + index + '"]');
-        const imageFavorites = commentObj.querySelector('.heart-svg');
-
-        if (comment.is_favorite) {
-            imageFavorites.src = firstImage;
-        }
-        else if (!comment.is_favorite) {
-            imageFavorites.src = secondImage;
-        }
-        
-        comment.answers.forEach(function(answer, index) {
-            let answerObj = commentObj.querySelector('[data-index="' + index + '"]');
-            const answerImage = answerObj.querySelector('.heart-svg');
-
-            if (answer.is_favorite) {
-                answerImage.src = firstImage;
-            }
-            else if (!answer.is_favorite) {
-                answerImage.src = secondImage;
-            }
-        }) 
-    })
-}
-
-function drawFavorites() {
-    toggleFavorites = !toggleFavorites; // Переключение состояния
-
-    comments.forEach(function(comment, index) {
-        let commentObj = document.querySelector('[data-index="' + index + '"]');
-        const staticCommentFlex = commentObj.querySelector('.static-comment__flex');
-        const replyField = commentObj.querySelector('.reply-field')
-
-        if (toggleFavorites) {
-            if (comment.is_favorite !== true) {
-                staticCommentFlex.style.display = 'none';
-                replyField.style.marginTop = '0px';
-            }
-            
-            comment.answers.forEach(function(answer, index) {
-                let answerObj = commentObj.querySelector('[data-index="' + index + '"]');
-                
-                if (answer.is_favorite !== true) {
-                    answerObj.style.display = 'none';
-                }
-            })
+        if (indexLi == 0) {
+            replyField.style.flexDirection = 'column'
+            console.log('work')
         } 
-        else if (!toggleFavorites) {
-            
-            staticCommentFlex.style.display = 'flex';
-            replyField.style.marginTop = '30px';
-            
-            comment.answers.forEach(function(answer, index) {
-                let answerObj = commentObj.querySelector('[data-index="' + index + '"]');
-                answerObj.style.display = 'flex';
-            }) 
-            
-        }
     })
 }
